2aa967c337a0b89ffac24d19cc3a265b599f7c3b
elsantiwg <122948040+elsantiwg@users.noreply.github.com>
2024-04-22 15:31:35 -0500

    solucion de errores
    desarrollo

---
+++ SistemaVenta.AplicacionWeb/Models/ViewModels/VMRol.cs
@@ -0,0 +1,17 @@
+﻿using SistemaVenta.Entity;
+
+namespace Multired.AplicacionWeb.Models.ViewModels
+{
+    public class VMRol
+    {
+        public partial class Rol
+        {
+
+            public int IdRol { get; set; }
+            public string? Descripcion { get; set; }
+
+        }
+    }
+}
+
+
---
+++ SistemaVenta.BLL/Interfaces/IUtilidadesService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multired.BLL.Interfaces
+{
+    public interface IUtilidadesService
+    {
+
+        string GenerarClave();
+
+        string ConvertirSha512(string texto);
+
+    }
+}
