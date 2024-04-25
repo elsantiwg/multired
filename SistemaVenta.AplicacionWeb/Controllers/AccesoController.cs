3c031fb6dd844aae68f0680cef90b9e839b1da55
Ja1deR <112580457+Ja1deR@users.noreply.github.com>
2024-04-24 21:58:24 -0500

    login_persistente

--- SistemaVenta.AplicacionWeb/Controllers/AccesoController.cs
+++ SistemaVenta.AplicacionWeb/Controllers/AccesoController.cs
@@ -1,12 +1,74 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
+
+using Multired.AplicacionWeb.Models.ViewModels;
+using Multired.BLL.Interfaces;
+using SistemaVenta.Entity;
+
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+
 namespace Multired.AplicacionWeb.Controllers
 {
     public class AccesoController : Controller
     {
-        public IActionResult Index()
+        private readonly IUsuarioService _usuarioServicio;
+
+        public AccesoController(IUsuarioService usuarioServicio)
         {
+            _usuarioServicio = usuarioServicio;
+        }
+
+        public IActionResult Login()
+        {
+            ClaimsPrincipal claimUser = HttpContext.User;
+
+            if(claimUser.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Index", "Home");
+            }
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(VMUsuarioLogin modelo)
+        {
+
+            Usuario usuario_encontrado = await _usuarioServicio.ObtenerPorCredenciales(modelo.Correo, modelo.Clave);
+            
+            if(usuario_encontrado == null)
+            {
+                ViewData["Mensaje"] = "no se encontraron coincicidencias";
+                return View();
+            }
+
+            ViewData["Mensaje"] = null;
+
+            List<Claim> claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name, usuario_encontrado.Nombre),
+                new Claim(ClaimTypes.NameIdentifier, usuario_encontrado.IdUsuario.ToString()),
+                new Claim(ClaimTypes.Role, usuario_encontrado.IdRol.ToString()),
+                new Claim("UrlFoto", usuario_encontrado.UrlFoto),
+            };
+
+
+            ClaimsIdentity claimsIdentity = new ClaimsIdentity(claims,CookieAuthenticationDefaults.AuthenticationScheme);
+
+            AuthenticationProperties properties = new AuthenticationProperties()
+            {
+                AllowRefresh = true,
+                IsPersistent = modelo.MantenerSesion
+            };
+
+            await HttpContext.SignInAsync(
+                CookieAuthenticationDefaults.AuthenticationScheme,
+                new ClaimsPrincipal(claimsIdentity),
+                properties
+                );
+
+            return RedirectToAction("Index","Home");
         }
     }
 }
